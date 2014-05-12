e43b67f78ab69fcd6b02024900dc77284a0b431d
Alexander Nezhinsky <alexander@riverscale.com>
2014-05-12 18:49:59 +0300

    ver 0.3.5

--- itchygen.h
+++ itchygen.h
@@ -17,7 +17,7 @@ extern "C" {
 #define likely(x)	__builtin_expect(!!(x), 1)
 #define unlikely(x)	__builtin_expect((x), 0)
 
-#define ITCHYGEN_VER_STR	"0.3.4"
+#define ITCHYGEN_VER_STR	"0.3.5"
 
 void version(void);
 void usage(int status, char *msg);
